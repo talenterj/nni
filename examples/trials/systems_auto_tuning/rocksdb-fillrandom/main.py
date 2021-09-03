6bf1f75707e166186a10abe5173bc381ea09343d
CheneyDing <1017472599@qq.com>
2021-09-03 20:33:31 +0800

    global declare in func then gave it a value ,not same time

--- examples/trials/systems_auto_tuning/rocksdb-fillrandom/main.py
+++ examples/trials/systems_auto_tuning/rocksdb-fillrandom/main.py
@@ -91,8 +91,10 @@ def run(**parameters):
 
     #cpu_trial = (int)(mean(list_cpu) * 10) / 10
     #memory_trial = (int)(mean(list_mem) / 1024 / 1024 / 1024 * 100) / 100
-    global cpu_trial = 1
-    global cpu_trial = 1
+    global cpu_trial 
+    cpu_trial = 1 
+    global memory_trial
+    memory_trial = 1
     # split into lines
     lines = out.decode("utf8").splitlines()
 
