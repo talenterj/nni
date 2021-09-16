15e034c38fc2ffe3856c07b4df4b1f680436192e
CheneyDing <1017472599@qq.com>
2021-09-16 15:43:36 +0800

    test readrandom workload

--- examples/trials/systems_auto_tuning/rocksdb-fillrandom/main.py
+++ examples/trials/systems_auto_tuning/rocksdb-fillrandom/main.py
@@ -138,7 +138,7 @@ def run(**parameters):
 def generate_params(received_params):
     '''generate parameters based on received parameters'''
     params = {
-        "benchmarks": "fillrandom",
+        "benchmarks": "readrondom",
         "threads": 1,
         "key_size": 16,
         "value_size": 100,
